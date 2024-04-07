7f0ba9e99bb7a18de69fd2889d226690d40e8e50
guillsil <guasilva@fi.uba.ar>
2024-04-06 22:36:37 -0300

    update

--- js/main-v2.js
+++ js/main-v2.js
@@ -25,6 +25,61 @@ document.getElementById("monto").addEventListener("input", function() {
     }
 });
 
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const media = window.matchMedia('(width < 40em)');
+const topNavMenu = document.querySelector('.topnav__menu');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
+
+// Contador para el número de inputs agregados
+let inputCount = 4; // Iniciamos en 4 porque ya hay 4 inputs presentes inicialmente
+
+
+function openMobileMenu() {
+    btnOpen.setAttribute('aria-expanded', 'true');
+    topNavMenu.removeAttribute('inert');
+    topNavMenu.removeAttribute('style');
+    main.setAttribute('inert', '');
+    bodyScrollLockUpgrade.disableBodyScroll(body);
+    btnClose.focus();
+  }
+  
+  function closeMobileMenu() {
+    btnOpen.setAttribute('aria-expanded', 'false');
+    topNavMenu.setAttribute('inert', '');
+    main.removeAttribute('inert');
+    bodyScrollLockUpgrade.enableBodyScroll(body);
+    btnOpen.focus();
+  
+    setTimeout(() => {
+      topNavMenu.style.transition = 'none';
+    }, 500);
+  }
+  
+  function setupTopNav(e) {
+    if (e.matches) {
+      // is mobile
+      console.log('is mobile');
+      topNavMenu.setAttribute('inert', '');
+      topNavMenu.style.transition = 'none';
+    } else {
+      // is tablet/desktop
+      console.log('is desktop');
+      closeMobileMenu();
+      topNavMenu.removeAttribute('inert');
+    }
+  }
+  
+  setupTopNav(media);
+  
+  btnOpen.addEventListener('click', openMobileMenu);
+  btnClose.addEventListener('click', closeMobileMenu);
+  
+  media.addEventListener('change', function (e) {
+    setupTopNav(e);
+  });
+
 
 function showAddInput() {
     var label = document.querySelector(".inputs-seccion .input-seccion__odd-label__tres.hidden");
