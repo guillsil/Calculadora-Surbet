94ef57f14c60b2cd6c3644693170b4a65d1dc0cf
guillsil <guasilva@fi.uba.ar>
2024-04-06 21:48:01 -0300

    new update

--- js/main-v2.js
+++ js/main-v2.js
@@ -133,80 +133,6 @@ function isAmountFilled() {
     return document.getElementById("monto").value.trim() !== "";
 }
 
-
-  
-
-
-
-function calculate2() {
-    var odd1 = parseFloat(document.getElementById("input1").value);
-    var odd2 = parseFloat(document.getElementById("input2").value);
-    var odd3 = parseFloat(document.getElementById("input3").value); 
-    var stake = parseFloat(document.getElementById("monto").value);
-
-    var result = 1 / odd1 + 1 / odd2 + (odd3 ? 1 / odd3 : 0); // Solo incluir odd3 si está presente
-    var isSurebet = result < 1 ? true : false;
-
-    // Normalizar las probabilidades
-    var normalOdd1 = (1 / odd1) / result;
-    var normalOdd2 = (1 / odd2) / result;
-    var normalOdd3 = odd3 ? (1 / odd3) / result : 0; // Normalizar solo si odd3 está presente
-
-    // Ganancia en cada apuesta
-    var ganancia1 = stake / normalOdd1;
-    var ganancia2 = stake / normalOdd2;
-    var ganancia3 = odd3 ? stake / normalOdd3 : 0; // Calcular ganancia solo si odd3 está presente
-
-    // Calculo de la cantidad a apostar
-    var howMuchbet_1 = stake * normalOdd1;
-    var howMuchbet_2 = stake * normalOdd2;
-    var howMuchbet_3 = odd3 ? stake * normalOdd3 : 0; // Calcular solo si odd3 está presente
-
-    // Calculo de la ganancia de cada apuesta
-    var winning_1 = howMuchbet_1 * odd1 - stake;
-    var winning_2 = howMuchbet_2 * odd2 - stake;
-    var winning_3 = howMuchbet_3 * odd3 - stake;
-
-    // Calculo de la ganancia total
-    var totalWinning = winning_1 + winning_2 ;
-    var profitPercentage = 0;
-
-    // Solo incluir la ganancia de la tercera apuesta si odd3 está presente
-    if (odd3) {
-        totalWinning += winning_3;
-        profitPercentage = (totalWinning / (stake * 3)) * 100;
-    }else{
-        totalWinning = totalWinning/2;
-        profitPercentage = (totalWinning / (stake * 2)) * 100*2;
-    }
-
-
-    // Redondeo
-    howMuchbet_1 = Math.round(howMuchbet_1);
-    howMuchbet_2 = Math.round(howMuchbet_2);
-    howMuchbet_3 = Math.round(howMuchbet_3);
-    totalWinning = Math.round(totalWinning);
-    profitPercentage = Math.round(profitPercentage);
-    
-    // Actualización de los elementos HTML con los resultados
-    document.getElementById("resultado-odd1").textContent = howMuchbet_1.toLocaleString('es-ES', { minimumFractionDigits: 2 });
-    document.getElementById("resultado-odd2").textContent = howMuchbet_2.toLocaleString('es-ES', { minimumFractionDigits: 2 });
-    document.getElementById("resultado-odd3").textContent = howMuchbet_3.toLocaleString('es-ES', { minimumFractionDigits: 2 });
-    document.getElementById("resultado-ganancia").textContent = totalWinning.toLocaleString('es-ES', { minimumFractionDigits: 2 });
-    document.getElementById("resultado-porcentaje").textContent = profitPercentage.toLocaleString('es-ES', { minimumFractionDigits: 2 });
-
-    // Mostrar si es surebet o no
-    var specialButton = document.getElementById("specialButton");
-    if (isSurebet) {
-        specialButton.textContent = "Surebet";
-        specialButton.style.backgroundImage = "linear-gradient(90deg, hsl(45, 100%, 50%), hsl(220, 100%, 15%))";
-        
-    } else {
-        specialButton.textContent = "No Surebet";
-        specialButton.style.backgroundImage = "";
-    }
-}
-
 function calculate() {
     var stake = parseFloat(document.getElementById("monto").value);
     var totalWinning = 0;
@@ -251,13 +177,9 @@ function calculate() {
     if (isSurebet) {
         specialButton.textContent = "Surebet";
         specialButton.style.backgroundImage = "hsl(20.62, 100%, 62.35%)";
-        newSpecialButton.textContent = "Surebet";
-        newSpecialButton.style.backgroundImage = "hsl(20.62, 100%, 62.35%)";
     } else {
         specialButton.textContent = "No Surebet";
-        specialButton.style.backgroundImage = "linear-gradient(to right, hsl(45, 100%, 50%), hsl(220, 100%, 15%))";
-        newSpecialButton.textContent = "No Surebet";
-        newSpecialButton.style.backgroundImage = "linear-gradient(to right, hsl(45, 100%, 50%), hsl(220, 100%, 15%))";
+        specialButton.style.backgroundImage = "hsl(0, 100%, 50%)";
     }
 }
 
